09024d1e605057eff50397951bcb9fe7a9a1733e
CHIHAN GEEKER <chihan597@gmail.com>
2021-02-02 23:24:07 +0800

    Hello no sleep 2/2/2021

--- app/src/main/java/com/example/sdp_bfit/MainActivity.java
+++ app/src/main/java/com/example/sdp_bfit/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.sdp_bfit;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -23,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String[] CAMERA_PERMISSION = new String[]{Manifest.permission.CAMERA};
     private static final int CAMERA_REQUEST_CODE = 10;
+    private double MagnitudePrevious =0;
+    public static Integer stepCount = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,9 +52,52 @@ public class MainActivity extends AppCompatActivity {
         TextView textView = view.findViewById(R.id.calories);
         textView.setText("hi");
 
+        //stepcounter
+        SensorManager sensorManager=(SensorManager)getSystemService(SENSOR_SERVICE);
+        Sensor sensor=sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+
+
+        SensorEventListener stepDetector=new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                if (sensorEvent!=null){
+                    float x_acceleration = sensorEvent.values[0];
+                    float y_acceleration = sensorEvent.values[1];
+                    float z_acceleration = sensorEvent.values[2];
+
+                    double Magnitude = Math.sqrt(x_acceleration*x_acceleration + y_acceleration*y_acceleration + z_acceleration*z_acceleration);
+                    double MagnitudeDelta = Magnitude-MagnitudePrevious;
+                    MagnitudePrevious=Magnitude;
+
+                    if (MagnitudeDelta> 6){
+                        stepCount++;
+
+                    }
+
+
+                }
+
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
+        };
+        sensorManager.registerListener(stepDetector,sensor,SensorManager.SENSOR_DELAY_NORMAL);
+
+
+
+
+
 
 
     }
+
+
+
+
     public boolean hasCameraPermission() {
         return ContextCompat.checkSelfPermission(
                 this,
